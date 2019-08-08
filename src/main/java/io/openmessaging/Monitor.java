8103306b08ef56206444740c9805cff198611507
yanghuiwei <yanghuiwei@meituan.com>
2019-08-08 09:51:08 +0800

    调参 INDEX_INTERVAL = 64 INDEX_ELE_LENGTH = 1024 * 512;

--- src/main/java/io/openmessaging/Monitor.java
+++ src/main/java/io/openmessaging/Monitor.java
@@ -34,6 +34,7 @@ public class Monitor {
 
     public static void log() {
         StringBuilder sb = new StringBuilder();
+        sb.append("INDEX_INTERVAL:").append(Const.INDEX_INTERVAL).append(" INDEX_ELE_LENGTH").append(Const.INDEX_ELE_LENGTH).append("\n");
         sb.append("put cost time:").append(getMsgStartTime - putStartTime)
                 .append(",get msg cost time:").append(getAvgStartTime - getMsgStartTime)
                 .append(",get avg cost time:").append(System.currentTimeMillis() - getAvgStartTime);
