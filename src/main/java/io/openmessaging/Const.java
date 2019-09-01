ffd8b8ab3e08c87c9e42334780d161be9b0fe25c
yanghuiwei <yanghuiwei@meituan.com>
2019-09-01 18:47:06 +0800

    test 27 A_INDEX_INTERVAL = 16

--- src/main/java/io/openmessaging/Const.java
+++ src/main/java/io/openmessaging/Const.java
@@ -35,16 +35,16 @@ public interface Const {
 
     boolean PRINT_LOG = true;
 
-    int MERGE_T_TIME = 28;
+    int MERGE_T_TIME = 27;
     int MERGE_T_INDEX_INTERVAL = 512 * MERGE_T_TIME;
     int MERGE_T_INDEX_LENGTH = 31000 * 128 / MERGE_T_TIME;
-    int A_INDEX_INTERVAL = 32;
+    int A_INDEX_INTERVAL = 16;
     int A_INDEX_LENGTH = MERGE_T_INDEX_LENGTH * (MERGE_T_INDEX_INTERVAL / A_INDEX_INTERVAL); //BLOCK_INDEX_SIZE的倍数
 
     int FILE_NUMS = 16;
     int T_MEMORY_SIZE = 1024 * 1024 * 252;
 
-    int DEST = 25500;
+    int DEST = 26000;
 
     int GET_THREAD_NUM = 24;
 }
