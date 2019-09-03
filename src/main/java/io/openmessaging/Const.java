6b53a91be5b31a2731c9d29bb6df7ea1298a6105
yanghuiwei <yanghuiwei@meituan.com>
2019-09-03 12:48:44 +0800

    test 3

--- src/main/java/io/openmessaging/Const.java
+++ src/main/java/io/openmessaging/Const.java
@@ -35,10 +35,9 @@ public interface Const {
     boolean PRINT_LOG = true;
 
     int[] T_INDEX_INTERVALS = new int[]{
-            1024 * 48,
-            1024 * 24,
-            1024 * 12,
-            1024 * 6
+            1024 * 36,
+            1024 * 18,
+            1024 * 9
     };
     int MERGE_T_TIME = 24;
     int MAX_T_INDEX_INTERVAL = 1024 * MERGE_T_TIME;
